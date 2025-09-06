b7df979730fdefb508b76d550cc23baf0010ee22
wu <wu@.com>
2025-09-06 19:07:13 +0800

    run ros2/ros2_documentation/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os.path
+from setuptools import setup
 
-from tos import VERSION
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-readme = f.read()
-f.close()
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    readme = f.read()
 
 setup(
-    name='django-tos',
-    version=".".join(map(str, VERSION)),
-    description='django-tos is a reusable Django application for setting Terms of Service.',
+    name="sphinx-multiversion",
+    description="Add support for multiple versions to sphinx",
     long_description=readme,
-    author='Frank Wiles',
-    author_email='frank@revsys.com',
-    url='http://github.com/revsys/django-tos/tree/master',
-    packages=find_packages(),
-    include_package_data=True,
+    long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
+    author="Jan Holthuis",
+    author_email="holthuis.jan@googlemail.com",
+    url="https://holzhaus.github.io/sphinx-multiversion/",
+    version="0.2.4",
+    install_requires=["sphinx >= 2.1"],
+    license="BSD",
+    packages=["sphinx_multiversion"],
+    entry_points={
+        "console_scripts": ["sphinx-multiversion=sphinx_multiversion:main",],
+    },
 )
